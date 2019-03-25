618ccb3b25015da5e22a88b5a724436cc9a182c3
杨杰 <jieyangb@ctrip.com>
2019-03-25 21:29:09 +0800

    -功能[ 定时器 ] -备注[ ]

--- iOSSweetSugar/iOSSweetSugar/dispatch定时器/MMDispatchTimer.h
+++ iOSSweetSugar/iOSSweetSugar/dispatch定时器/MMDispatchTimer.h
@@ -12,6 +12,36 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface MMDispatchTimer : NSObject
 
+/**
+ 构造定时器
+ 
+ @param task block
+ @param startT 多少秒后执行
+ @param interval 重复时间
+ @param isRepeat 是否重复
+ @param isAsync 是否异步
+ @return 对应的timer的key。
+ */
++ (NSString *)excuTimerTask:(void(^)(void))task andStart:(NSTimeInterval)startT interval:(NSTimeInterval)interval repeats:(BOOL)isRepeat async:(BOOL)isAsync;
+
+/**
+ sel定时器
+ 
+ @param task SEL
+ @param target target
+ @param startT 多少秒后执行
+ @param interval 重复时间
+ @param isRepeat 是否重复
+ @param isAsync 是否异步
+ @return 对应的timer的key。
+ */
++ (NSString *)excuTimerSelector:(SEL)task target:(id)target andStart:(NSTimeInterval)startT interval:(NSTimeInterval)interval repeats:(BOOL)isRepeat async:(BOOL)isAsync;
+/**
+ 取消定时器
+ 
+ @param name key
+ */
++ (void)cancelTask:(NSString *)name;
 @end
 
 NS_ASSUME_NONNULL_END
--- iOSSweetSugar/iOSSweetSugar/dispatch定时器/MMDispatchTimer.m
+++ iOSSweetSugar/iOSSweetSugar/dispatch定时器/MMDispatchTimer.m
@@ -7,7 +7,62 @@
 //
 
 #import "MMDispatchTimer.h"
+#import <pthread.h>
 
 @implementation MMDispatchTimer
 
+
+static NSMutableDictionary *timers_;
+
+pthread_mutex_t mutex_l;
+
++ (void)initialize {
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        timers_ = [NSMutableDictionary dictionary];
+        pthread_mutex_init(&mutex_l, NULL);
+    });
+}
++ (NSString *)excuTimerTask:(void(^)(void))task andStart:(NSTimeInterval)startT interval:(NSTimeInterval)interval repeats:(BOOL)isRepeat async:(BOOL)isAsync {
+    if (!task || startT < 0 || (interval <= 0 && isRepeat)) return nil;
+    dispatch_queue_t queue = isAsync? dispatch_get_global_queue(0, 0):dispatch_get_main_queue();
+    dispatch_source_t timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, queue);
+    dispatch_source_set_timer(timer, dispatch_time(DISPATCH_TIME_NOW, startT * NSEC_PER_SEC), interval * NSEC_PER_SEC, 0);
+    pthread_mutex_lock(&mutex_l);
+    NSString *timerN = [NSString stringWithFormat:@"timers_%ld",(unsigned long)timers_.count];
+    [timers_ setObject:timer forKey:timerN];
+    pthread_mutex_unlock(&mutex_l);
+    dispatch_source_set_event_handler(timer, ^{
+        task();
+        if (!isRepeat) {
+            [self cancelTask:timerN];
+        }
+    });
+    dispatch_resume(timer);
+    return timerN;
+}
++ (NSString *)excuTimerSelector:(SEL)task target:(id)target andStart:(NSTimeInterval)startT interval:(NSTimeInterval)interval repeats:(BOOL)isRepeat async:(BOOL)isAsync {
+    if (!task || !target) return nil;
+    
+    return [self excuTimerTask:^{
+        if ([target respondsToSelector:task]) {
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Warc-performSelector-leaks"
+            [target performSelector:task];
+#pragma clang diagnostic pop
+        }
+    } andStart:startT interval:interval repeats:isRepeat async:isAsync];
+}
++ (void)cancelTask:(NSString *)name {
+    if (name.length <= 0) {
+        return;
+    }
+    dispatch_source_t timer = [timers_ objectForKey:name];
+    if (timer) {
+        dispatch_source_cancel(timer);
+        pthread_mutex_lock(&mutex_l);
+        [timers_ removeObjectForKey:name];
+        pthread_mutex_unlock(&mutex_l);
+    }
+}
 @end
